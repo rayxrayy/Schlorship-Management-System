0409f363c4513eb3fb79cf696ef018ffbc6badb4
Gayatri <np03ca4a210022@heraldcollege.edu.np>
2023-12-18 22:54:38 +0545

    Landing Page updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,21 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <style>
+         body {
+            transition: background-color 0.3s, color 0.3s;
+            /* background: url("{{ asset('./Images/images.png') }}"); */
+        }
+
+        .dark-mode {
+            background-color: #212124; /* Dark background color */
+            color: #fff; /* Light text color */
+        }
         .button-container {
             display: flex;
             justify-content: flex-end;
-            margin-top: 20px;
-            margin-right: 20px;
+            margin-top: 40px;
+            margin-right: 40px;
+            font-size: 18px;
         }
         .button-container a {
             margin-left: 10px;
@@ -31,32 +41,69 @@
         .footer-container {
             display: flex;
             justify-content: space-between;
-            margin-top: 20px; /* Adjust this value to control the space between buttons and footer content */
+            margin-top: 100px; /* Adjust this value to control the space between buttons and footer content */
+            font-size: 18px;
+        }
+          /* Mobile-specific styles */
+          @media only screen and (max-width: 600px) {
+            .button-container {
+                justify-content: center; /* Center buttons on small screens */
+                margin-right: 0; /* Remove right margin on small screens */
+            }
+
+            .footer-container {
+                justify-content: center; /* Center content in the footer on small screens */
+                margin-right: 0; /* Remove right margin on small screens */
+            }
         }
     </style>
 </head>
 <body class="antialiased">
+    <!-- Logo -->
+    <div style="position: absolute; top: 10px; left: 100px;">
+        <a href="#">
+        <img src="{{ asset('./Images/Logo.png') }}" alt="Logo"  style="width: 100px; height: auto;"/>
+        </a>
+</div>
+
     <div class="button-container">
+        <a href="#" onclick="toggleDarkMode()"style="margin-left: 10px;">Blog </a>
+        <a href="#" onclick="toggleDarkMode()"style="margin-left: 50px;">Dark Mode</a>
         @if (Route::has('login'))
             <div>
                 @auth
                     <a href="{{ url('/dashboard') }}">Dashboard</a>
                 @else
-                    <a href="{{ route('login') }}">Log in</a>
+                    <a href="{{ route('login') }}" style="margin-left: 50px;">Log in</a>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}"> Register</a>
+                        <a href="{{ route('register') }}" style="margin-left: 50px;"> Register</a>
                     @endif
                 @endauth
             </div>
         @endif
+        <a href="#" onclick="toggleDarkMode()"style="margin-left: 50px;">Dark Mode</a>
     </div>
-
+    <div style="display: flex;">
+        <img src="{{ asset('./Images/images.png') }}" alt="Description of Image 1" style="width: 50%;">
+        <img src="{{ asset('./Images/Images.png') }}" alt="Description of Image 2" style="width: 50%;">
+    </div>
+    <div style="display: flex;">
+        <img src="{{ asset('./Images/images.png') }}" alt="Description of Image 1" style="width: 50%;">
+        <img src="{{ asset('./Images/Images.png') }}" alt="Description of Image 2" style="width: 50%;">
+    </div>
     <div class="footer-container">
         <div>
-            <a href="https://github.com/rayxrayy/Schlorship-Management-System">Sponsor</a>
+            <a href="https://github.com/rayxrayy/Schlorship-Management-System" target="_blank" style="margin-left: 50px;">Sponsor</a>
+
         </div>
-        <div>Edu-Hope</div>
+        <div style= "margin-right: 100px; margin-bottom: 50px;">Edu-Hope</div>
     </div>
+    <script>
+        // Toggle dark mode
+        function toggleDarkMode() {
+            document.body.classList.toggle('dark-mode');
+        }
+    </script>
 </body>
 </html>
